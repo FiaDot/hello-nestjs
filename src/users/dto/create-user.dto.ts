ad4f636aaac6bef949255ec8f1bf43902bf73200
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-03-30 15:03:00 +0900

    create user swagger 적용

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: '이름',
+    example: '김초롱', // default: "string" 해당 타입이 들어감
+    required: true, // default: false
+  })
   name: string;
+
+  @ApiProperty({ description: '이메일' })
   email: string;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,9 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class User {
+  @ApiProperty({ description: '고유번호' })
+  id: string;
+
+  @ApiProperty({ description: '이메일' })
+  email: string;
+}
