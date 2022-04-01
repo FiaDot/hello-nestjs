a668aab23144f42a0268b5316a693f2cee7b7de9
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-01 16:51:55 +0900

    유저 생성 DB 처리

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const exists = await this.isUserExists(createUserDto.platformUID);
+    if (exists) {
+      throw new UnprocessableEntityException('이미 가입된 사용자 입니다.');
+    }
+
+    const user = new User();
+    user.platformUID = createUserDto.platformUID;
+    return this.userRepository.save(user);
   }
 
   findAll() {
@@ -14,6 +31,13 @@ export class UsersService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
+  }
+
+  async isUserExists(platformUID: string): Promise<boolean> {
+    const user = await this.userRepository.findOne({ where: { platformUID } });
+    console.log('isUserExists');
+    console.log(user);
+    return !!user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
