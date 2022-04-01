b27f3282d8d8f8e85d09dff09cf15ca5bd09cf84
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-01 16:01:08 +0900

    entity 정의

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity('User')
 export class User {
-  @ApiProperty({ description: '고유번호' })
-  id: string;
+  @ApiProperty({ description: '플랫폼 제공 User ID' })
+  @Column({ length: 128 })
+  platformUID: string;
 
-  @ApiProperty({ description: '이메일' })
-  email: string;
+  @ApiProperty({ description: '유저 고유번호' })
+  @PrimaryGeneratedColumn('uuid')
+  uid: string;
+
+  @ApiProperty({ description: '레벨' })
+  @Column({ default: 1 })
+  lv: number;
+
+  @ApiProperty({ description: '경험치' })
+  @Column({ default: 0 })
+  exp: number;
+
+  @ApiProperty({ description: '골드' })
+  @Column({ default: 0 })
+  gold: number;
+
+  @ApiProperty({ description: '차단여부' })
+  @Column({ default: false })
+  block: boolean;
+
+  @ApiProperty({ description: '최종 로그인 시간' })
+  @Column({ default: null, nullable: true })
+  loginAt: Date;
+
+  @ApiProperty({ description: '생성일시' })
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty({ description: '수정일시' })
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
