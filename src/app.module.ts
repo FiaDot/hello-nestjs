16202dd8c78e563b738ac8d94ec5441d9f308d0b
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-04 14:40:11 +0900

    app module timezone 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    UsersModule,
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: 'mysql',
+    //     host: 'localhost',
+    //     port: 13306,
+    //     username: 'everse',
+    //     password: 'asdf1234!',
+    //     database: 'everse',
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: true,
+    //     timezone: 'Asia/Seoul',
+    //   }),
+    // }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -16,8 +28,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'everse',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-      timezone: 'Asia/Seoul',
+      // timezone: 'Asia/Seoul',
+      // timezone: 'Z',
+      timezone: 'local',
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
