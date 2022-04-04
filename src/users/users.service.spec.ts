d547f6b1d7c41bc489c40f73e1e5bfbfb2d1dc81
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-04 14:50:00 +0900

    service에서 find, datetime update 기능

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,11 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Logger } from '@nestjs/common';
+
+if (global.console.constructor.name === 'CustomConsole') {
+  // you can also override the global.console with another CustomConsole of yours, like https://stackoverflow.com/a/57443150
+  global.console = require('console');
+}
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 13306,
+          username: 'everse',
+          password: 'asdf1234!',
+          database: 'everse',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+          //timezone: 'Asia/Seoul',
+          //timezone: 'Z',
+          timezone: 'local',
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
       providers: [UsersService],
     }).compile();
 
@@ -14,5 +39,22 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('date checker', async () => {
+    const createUserDto = new CreateUserDto();
+    createUserDto.platformUID = 'test14';
+
+    const user: User = await service.create(createUserDto);
+    expect(user.uid).toBeDefined();
+
+    console.log(`create login = ${user.loginAt}`);
+    console.log(`create create= ${user.createdAt}`);
+
+    const recordedUser = await service.findOneByPlatformUID(
+      createUserDto.platformUID,
+    );
+    console.log(`record login = ${recordedUser.loginAt}`);
+    console.log(`record create= ${recordedUser.createdAt}`);
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as moment from 'moment-timezone';
 
 @Injectable()
 export class UsersService {
@@ -19,13 +20,13 @@ export class UsersService {
 
     const user = new User();
     user.platformUID = createUserDto.platformUID;
+    user.loginAt = moment().utcOffset(9).toDate();
+
     return this.userRepository.save(user);
   }
 
   async isUserExists(platformUID: string): Promise<boolean> {
-    const user = await this.userRepository.findOne({ where: { platformUID } });
-    console.log('isUserExists');
-    console.log(user);
+    const user = await this.findOneByPlatformUID(platformUID);
     return !!user;
   }
 
@@ -35,6 +36,11 @@ export class UsersService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
+  }
+
+  async findOneByPlatformUID(platformUID: string) {
+    const user = await this.userRepository.findOne({ where: { platformUID } });
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
