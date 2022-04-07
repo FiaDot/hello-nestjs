1b6f1b0bc9facfbfb225970b29db1c125d8defc7
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-07 10:41:42 +0900

    mysql 접속 정보

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 13306,
+      username: 'everse',
+      password: 'asdf1234!',
+      database: 'everse',
+      // pool: {
+      //   max: 5,
+      //   min: 1,
+      //   acquire: 30000,
+      //   idle: 10000,
+      // },
+      dialectOptions: {
+        charset: 'utf8mb4',
+        // typeCast, dateStrings 2개의 옵션이 true가 아니면 select 시에 시간이 UTC로 나옴
+        // dateStrings: true,
+        // typeCast: true,
+      },
+      models: [],
+    }),
     UsersModule,
   ],
   controllers: [AppController],
