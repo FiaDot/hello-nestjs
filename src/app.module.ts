2352891501e9473d68ec311e4e7db48e33a900fa
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-07 15:33:48 +0900

    sequelize 옵션

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -23,13 +22,14 @@ import { User } from './users/entities/user.entity';
       dialectOptions: {
         charset: 'utf8mb4',
         // typeCast, dateStrings 2개의 옵션이 true가 아니면 select 시에 시간이 UTC로 나옴
-        // dateStrings: true,
-        // typeCast: true,
+        dateStrings: true, // 날짜를 string으로 가져오기
+        typeCast: true, // 날짜의 경우 타입 캐스팅
       },
+      timezone: '+09:00', // Asia/Seoul
       logging: true,
       synchronize: true,
-      // autoLoadModels: true, // models 정의 할 필요없이 전체 엔티티 검색해서 등록
-      models: [User],
+      autoLoadModels: true, // models 정의 할 필요없이 전체 엔티티 검색해서 등록
+      // models: [User], // 수동으로 추가했을 때 테이블 생성 안됨
     }),
     UsersModule,
   ],
