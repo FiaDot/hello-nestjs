a834cccb04a17ca2b46a1f1d3093e8a365167f98
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-07 16:41:05 +0900

    DB 연결없이 테스트

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,7 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { DateTimeHelper } from '../common/helpers/datetime.helper';
 import { AppModule } from '../app.module';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
 
 // 로그 상세 정보 끄기
 if (global.console.constructor.name === 'CustomConsole') {
@@ -26,21 +26,59 @@ if (global.console.constructor.name === 'CustomConsole') {
 //   },
 // };
 
+// const mockModel = {
+//   findByPk: jest.fn((id) => {
+//     const testUser = new User();
+//     testUser.id = id;
+//     testUser.platformUID = 'test';
+//     return testUser;
+//   }),
+// };
+
+const mockUserModel = () => ({
+  findByPk: jest.fn((id) => {
+    const testUser = new User();
+    testUser.id = id;
+    testUser.platformUID = 'test';
+    return testUser;
+  }),
+});
+
+const testUser = { id: 1, platformUID: 'test' };
+
 describe('UsersService', () => {
   let service: UsersService;
+  let model: typeof User;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       // 실제 DB에 접속해서 하는 방법
-      imports: [AppModule, SequelizeModule.forFeature([User])],
-      providers: [UsersService],
+      // imports: [SequelizeModule.forFeature([User])],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User),
+          //useValue: mockModel,
+          //useFactory: mockUserModel,
+          useValue: {
+            findByPk: jest.fn((id) => testUser),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    model = module.get<typeof User>(getModelToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findOne', async () => {
+    const user: User = await service.findOne(1);
+    console.log(`user=${JSON.stringify(user)}`);
+    expect(user.id).toBeDefined();
   });
 
   it('createUser ', async () => {
