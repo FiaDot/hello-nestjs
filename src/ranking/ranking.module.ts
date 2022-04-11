2042488cd9a0ddfb43f5a09e2faa2ac88dfae417
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-11 18:34:17 +0900

    랭킹에서 redis 모듈 추가

--- src/ranking/ranking.module.ts
+++ src/ranking/ranking.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { RankingService } from './ranking.service';
 import { RankingController } from './ranking.controller';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 16379,
+    }),
+  ],
   controllers: [RankingController],
-  providers: [RankingService]
+  providers: [RankingService],
 })
 export class RankingModule {}
