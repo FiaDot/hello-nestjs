dc3abe15b639d404cd1be3120393774409984c3a
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-13 16:05:30 +0900

    컨트롤러 swagger 주석 추가

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -4,6 +4,7 @@ import {
   HealthCheckService,
   HttpHealthIndicator,
 } from '@nestjs/terminus';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('health-check')
 export class HealthCheckController {
@@ -12,6 +13,10 @@ export class HealthCheckController {
     private http: HttpHealthIndicator,
   ) {}
 
+  @ApiOperation({
+    summary: '헬스 체크 API',
+    description: '각종 서버의 상태를 체크',
+  })
   @Get()
   @HealthCheck()
   check() {
