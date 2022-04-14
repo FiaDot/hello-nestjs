73799dd1545c9ab63a0ff226629503cc774a6bbd
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-14 12:16:55 +0900

    auth 유닛 테스트 정상 동작화

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+
+
+const encoded = {
+  id: 1,
+  platformUID: '123',
+};
+
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((type) => {
+              switch (type) {
+                case 'JWT_EXPIRATION_TIME':
+                  return 100;
+                default:
+                  return null;
+              }
+            }),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn((payload) => encoded),
+          },
+        },
+        {
+          provide: UsersService,
+          useValue: {
+            findOneByPlatformUID: jest.fn(() => encoded),
+          },
+        },
+      ],
       controllers: [AuthController],
     }).compile();
 
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtModuleOptions } from '@nestjs/jwt/dist/interfaces/jwt-module-options.interface';
+import { UsersService } from '../users/users.service';
+
+const encoded = {
+  id: 1,
+  platformUID: '123',
+};
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn((payload) => encoded),
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((type) => {
+              switch (type) {
+                case 'JWT_EXPIRATION_TIME':
+                  return 100;
+                default:
+                  return null;
+              }
+            }),
+          },
+        },
+        {
+          provide: UsersService,
+          useValue: {
+            findOneByPlatformUID: jest.fn(() => encoded),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
