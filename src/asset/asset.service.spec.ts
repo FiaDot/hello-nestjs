08ab78d16c201884c85bcc4ac1e322efc0b7fb12
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-14 12:03:06 +0900

    asset 유닛 테스트 정상 동작화

--- src/asset/asset.service.spec.ts
+++ src/asset/asset.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssetService } from './asset.service';
+import { CsvModule } from 'nest-csv-parser';
 
 describe('AssetService', () => {
   let service: AssetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CsvModule],
       providers: [AssetService],
     }).compile();
 
