a9e4a8cacc118d47229c11b29140c4d3d2bd5131
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-14 13:39:44 +0900

    health check 유닛 테스트 정상 동작화

--- src/health-check/health-check.controller.spec.ts
+++ src/health-check/health-check.controller.spec.ts
@@ -1,5 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthCheckController } from './health-check.controller';
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  SequelizeHealthIndicator,
+} from '@nestjs/terminus';
+import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
+
+const healthCheckExecutorMock: Partial<HealthCheckExecutor> = {
+  execute: jest.fn(),
+};
 
 describe('HealthCheckController', () => {
   let controller: HealthCheckController;
@@ -7,6 +17,15 @@ describe('HealthCheckController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthCheckController],
+      providers: [
+        HealthCheckService,
+        HttpHealthIndicator,
+        SequelizeHealthIndicator,
+        {
+          provide: HealthCheckExecutor,
+          useValue: healthCheckExecutorMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthCheckController>(HealthCheckController);
