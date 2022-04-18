758deb08033b0a7d5cb6295b88d0fe617498dafd
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-18 15:54:52 +0900

    slack 알림 끄기

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,18 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { NotifyService } from '../notify/notify.service';
+// import { NotifyService } from '../notify/notify.service';
 
 @Injectable()
 export class TaskService {
   private readonly logger = new Logger(TaskService.name);
-  constructor(private notifyService: NotifyService) {}
+  // constructor(private notifyService: NotifyService) {}
 
   // 10초 마다 호출
   @Cron('0 * * * * *', { name: 'CronTask' })
   handleCron() {
     this.logger.log('task called.');
-    (async () => {
-      await this.notifyService.notifyText('task called.');
-    })();
+    // (async () => {
+    //   await this.notifyService.notifyText('task called.');
+    // })();
   }
 }
